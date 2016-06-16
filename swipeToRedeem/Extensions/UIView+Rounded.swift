eb16e64fff2d466ec755c197935cfbc3591e1343
soulchild <soulchildsince2013@gmail.com>
2016-06-16 18:32:55 +0800

    Starting layout

---
+++ swipeToRedeem/Extensions/UIView+Rounded.swift
@@ -0,0 +1,45 @@
+//
+//  UIView+Rounded.swift
+//  swipeToRedeem
+//
+//  Created by Axel Kee on 16/06/2016.
+//  Copyright © 2016 Sweatshop. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIView {
+    
+    func roundCorners(corners: UIRectCorner, radius: CGFloat){
+        let bounds : CGRect = self.bounds
+        let maskPath : UIBezierPath = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSizeMake(radius, radius))
+        
+        let maskLayer : CAShapeLayer = CAShapeLayer()
+        maskLayer.frame = bounds
+        maskLayer.path = maskPath.CGPath
+        
+        self.layer.mask = maskLayer
+        
+        /*
+        let frameLayer : CAShapeLayer = CAShapeLayer()
+        frameLayer.frame = bounds
+        frameLayer.path = maskPath.CGPath
+        frameLayer.strokeColor = UIColor(red: 0.7, green: 0.7, blue: 0.7, alpha: 1.0).CGColor
+        frameLayer.fillColor = nil
+        
+        self.layer.addSublayer(frameLayer)
+         */
+    }
+    
+    func roundTopCorners(radius: CGFloat){
+        self.roundCorners([UIRectCorner.TopLeft, UIRectCorner.TopRight] , radius: radius)
+    }
+    
+    func roundBottomCorners(radius: CGFloat){
+        self.roundCorners([UIRectCorner.BottomLeft, UIRectCorner.BottomRight] , radius: radius)
+    }
+    
+    
+}
--- swipeToRedeem/ViewController.swift
+++ swipeToRedeem/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var couponTopView: UIView!
+    @IBOutlet weak var couponPerforationUpView: UIImageView!
+    @IBOutlet weak var couponBottomView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        // Set the background image tile on main view
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "Background")!)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        // Customize coupon top view
+        self.couponTopView.roundTopCorners(10.0)
+        
+        self.couponBottomView.frame = CGRectMake(self.couponTopView.frame.origin.x, couponPerforationUpView.frame.maxY - 1.0, self.couponTopView.frame.size.width, 200)
     }
 
     override func didReceiveMemoryWarning() {
