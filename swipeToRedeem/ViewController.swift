a72a1a56a21191a79fdd31c0a8da345614a83e7a
soulchild <soulchildsince2013@gmail.com>
2016-06-17 18:15:18 +0800

    Added swipe cursor
    Added swipe cursor

--- swipeToRedeem/ViewController.swift
+++ swipeToRedeem/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     
     var swipeAreaView : UIView = UIView()
     
+    var swipeCursorView : UIView = UIView()
     
     
     override func viewDidLoad() {
@@ -54,8 +55,9 @@ class ViewController: UIViewController {
         
         self.view.insertSubview(shadowView, belowSubview: self.swipeContainerView)
         
-        self.swipeAreaView.layer.cornerRadius = 24.0
+        self.swipeAreaView.layer.cornerRadius = 28.0
         
+        self.swipeCursorView.layer.cornerRadius = 23.0
     }
 
     override func didReceiveMemoryWarning() {
@@ -63,7 +65,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Initialize coupon view
+    // MARK: - View Initialization
     func initializeCouponView() {
         
         
@@ -91,8 +93,10 @@ class ViewController: UIViewController {
         swipeContentView.backgroundColor = UIColor.whiteColor()
         
         swipeAreaView.translatesAutoresizingMaskIntoConstraints = false
-        swipeAreaView.backgroundColor  = UIColor(red: 0.5, green: 0.3, blue: 0.3, alpha: 1.0)
+        swipeAreaView.backgroundColor  = UIColor(red: 0.855, green: 0.032, blue: 0.303, alpha: 1.0)
         
+        swipeCursorView.translatesAutoresizingMaskIntoConstraints = false
+        swipeCursorView.backgroundColor = UIColor.whiteColor()
         
         self.view.addSubview(self.couponTopView)
 
@@ -104,6 +108,7 @@ class ViewController: UIViewController {
         self.swipeContainerView.addSubview(swipePerforationView)
         self.swipeContainerView.addSubview(swipeContentView)
         self.swipeContentView.addSubview(swipeAreaView)
+        self.swipeAreaView.addSubview(swipeCursorView)
         
         let topPerforationWidth : CGFloat = UIImage(named: "PerforationUp")!.size.width
         let topPerforationHeight : CGFloat = UIImage(named: "PerforationUp")!.size.height
@@ -117,15 +122,17 @@ class ViewController: UIViewController {
                      "swipeContainerView" : self.swipeContainerView,
                      "swipePerforationView" : self.swipePerforationView,
                      "swipeContentView" : self.swipeContentView,
-                     "swipeAreaView" : self.swipeAreaView]
+                     "swipeAreaView" : self.swipeAreaView,
+                     "swipeCursorView" : self.swipeCursorView]
         let metrics = ["couponTopViewHeight" : 200.0,
-                       "swipeContainerViewHeight" : 80.0 ]
+                       "swipeContainerViewHeight" : 100.0,
+                       "leftRightMarginToSuper" : 20.0]
         var constraints = []
         var constraint : NSLayoutConstraint = NSLayoutConstraint()
         var format = ""
         
         
-        format = "|-30-[couponTopView]-30-|"
+        format = "|-(leftRightMarginToSuper)-[couponTopView]-(leftRightMarginToSuper)-|"
         constraints = NSLayoutConstraint.constraintsWithVisualFormat(format, options: NSLayoutFormatOptions(rawValue: 0), metrics: metrics, views: views)
         self.view.addConstraints(constraints as! [NSLayoutConstraint])
         
@@ -147,7 +154,7 @@ class ViewController: UIViewController {
         
         
         
-        format = "|-30-[swipeContainerView]-30-|"
+        format = "|-(leftRightMarginToSuper)-[swipeContainerView]-(leftRightMarginToSuper)-|"
         constraints = NSLayoutConstraint.constraintsWithVisualFormat(format, options: NSLayoutFormatOptions(rawValue: 0), metrics: metrics, views: views)
         self.view.addConstraints(constraints as! [NSLayoutConstraint])
         
@@ -176,11 +183,42 @@ class ViewController: UIViewController {
         constraints = NSLayoutConstraint.constraintsWithVisualFormat(format, options: NSLayoutFormatOptions(rawValue: 0), metrics: metrics, views: views)
         self.swipeContentView.addConstraints(constraints as! [NSLayoutConstraint])
         
-        format = "V:|-0-[swipeAreaView]-8-|"
+        format = "V:|-10-[swipeAreaView]-18-|"
         constraints = NSLayoutConstraint.constraintsWithVisualFormat(format, options: NSLayoutFormatOptions(rawValue: 0), metrics: metrics, views: views)
         self.swipeContentView.addConstraints(constraints as! [NSLayoutConstraint])
         
+        /*
+        format = "V:|-6-[swipeCursorView]-6-|"
+        constraints = NSLayoutConstraint.constraintsWithVisualFormat(format, options: NSLayoutFormatOptions(rawValue: 0), metrics: metrics, views: views)
+        self.swipeAreaView.addConstraints(constraints as! [NSLayoutConstraint])
         
+        constraint = NSLayoutConstraint(item: swipeCursorView, attribute: .Width, relatedBy: .Equal, toItem: swipeCursorView, attribute: .Height, multiplier: 1.0, constant: 0.0)
+        self.swipeAreaView.addConstraint(constraint)
+        
+        
+        format = "|-6-[swipeCursorView]"
+        let swipeCursorOriginXconstraints = NSLayoutConstraint.constraintsWithVisualFormat(format, options: NSLayoutFormatOptions(rawValue: 0), metrics: metrics, views: views)
+        self.swipeAreaView.addConstraints(swipeCursorOriginXconstraints as! [NSLayoutConstraint])
+ 
+        
+        self.swipeAreaView.removeConstraints(swipeCursorOriginXconstraints)
+        
+        */
+        
+
+        swipeCursorView.frame = CGRectMake(6.0, 6.0, 45.0, 45.0)
+        
+        print("swipe cursor view origin x \(swipeCursorView.frame.origin.x)")
+ 
+       
+        
+        
+        self.swipeCursorView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(ViewController.handleDrag(_:)) ))
+    }
+    
+    // MARK: - handle for action
+    func handleDrag(panGesture: UIPanGestureRecognizer){
+        print("dragging")
     }
 }
 
