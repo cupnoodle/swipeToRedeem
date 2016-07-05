b12138e2cab6a58f080a7e6c14ea01a35469a090
soulchild <soulchildsince2013@gmail.com>
2016-07-05 23:35:22 +0800

    Messing around with command line build/test
    Reference from
    https://github.com/codecov/example-swift/blob/master/test.sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#-workspace swipeToRedeem.xcworkspace
+
+TEST_CMD="xcodebuild -scheme swipeToRedeemUITests -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.3' test"
+
+which -s xcpretty
+XCPRETTY_INSTALLED=$?
+
+if [[ $TRAVIS || $XCPRETTY_INSTALLED == 0 ]]; then
+  eval "${TEST_CMD} | xcpretty"
+else
+eval "$T
