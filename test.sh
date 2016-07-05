0715df2f65ad34de611ce93519edd9754e4cd915
soulchild <soulchildsince2013@gmail.com>
2016-07-06 00:07:52 +0800

    Putting backtick was a mistake...
    Putting backtick was a mistake...

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ which -s xcpretty
 XCPRETTY_INSTALLED=$?
 
 if [[ $TRAVIS || $XCPRETTY_INSTALLED == 0 ]]; then
-  eval `${TEST_CMD} | xcpretty`
+  eval "${TEST_CMD} | xcpretty"
 else
-  eval `$TEST_CMD`
+  eval "$TEST_CMD"
 fi
